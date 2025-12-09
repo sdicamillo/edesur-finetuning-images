a5d131d2b6d73bd4c1c73b172180d39d5b819c59
Santiago Di Camillo <santiagodc@signumenergy.com.ar>
2025-12-09 15:29:32 -0300

    subo imagenes para finetuning

---
+++ upload-github.bat
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://github.com/sdicamillo/edesur-finetuning-images.git
+git checkout -b main
+
+git add .
+git commit -m "subo imagenes para finetuning"
+git push origin main --force
